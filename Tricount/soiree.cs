7c9c1908a4a249051379fb9c7e017aa8acbd3aed
Enzo <enzo.gouerec@epsi.fr>
2021-12-13 11:18:30 +0100

    class soirée et user

---
+++ Tricount/soiree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tricount
+{
+    public class soiree
+    {
+        public int id { get; set; }
+
+        public int nb_participant { get; set; }
+
+        public int total_soiree { get; set; }
+
+        public int moyenne_utilisateur { get; set; }
+
+        public soiree(int NB_Participant, int Total_Soiree, int Moyenne_Utilisateur)
+        {
+            nb_participant = NB_Participant;
+            total_soiree = Total_Soiree;
+            moyenne_utilisateur = Moyenne_Utilisateur;
+        }
+
+        public soiree(int ID, int NB_Participant, int Total_Soiree, int Moyenne_Utilisateur)
+            : this(NB_Participant, Total_Soiree, Moyenne_Utilisateur)
+        {
+            id = ID;
+        }
+    }
+}
---
+++ Tricount/utilisateur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tricount
+{
+    public class utilisateur
+    {
+        public int id { get; set; }
+
+        public int nom { get; set; }
+
+        public int id_organisteur { get; set; }
+
+        public int dettes { get; set; }
+
+        public int depenses { get; set; }
+
+        public int id_soiree { get; set; }
+
+        public utilisateur(int Nom, int ID_Organisateur, int Dettes, int Depenses, int ID_Soiree)
+        {
+            nom = Nom;
+            id_organisteur = ID_Organisateur;
+            dettes = Dettes;
+            depenses = Depenses;
+            id_soiree = ID_Soiree;
+        }
+
+        public utilisateur(int ID, int Nom, int ID_Organisateur, int Dettes, int Depenses, int ID_Soiree)
+            : this(Nom, ID_Organisateur, Dettes, Depenses, ID_Soiree)
+        {
+            id = ID;
+        }
+    }
+}
